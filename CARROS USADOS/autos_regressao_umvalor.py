7c66eb22a3bee252e2db6fd12b06fb0b3d1b6b82
Felipe Honorato <felipesousa@eng.ci.ufpb.br>
2020-02-08 00:06:43 -0300

    PREÇO CARROS DATABASE

---
+++ CARROS USADOS/autos_regressao_umvalor.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+dados = pd.read_csv('autos', encoding = 'ISO-8859-1')
--- IRIS/irissaving.py
+++ IRIS/irissaving.py
@@ -1,19 +1,31 @@
 from keras.models import Sequential
 from keras.layers import Dense,Dropout
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import np_utils
 
+
+dados = pd.read_csv('iris.csv')
+previsores = dados.iloc[:,0:4].values
+classe = dados.iloc[:,4].values
+
+labEncoder = LabelEncoder()
+classe = labEncoder.fit_transform(classe)
+classe_dummy = np_utils.to_categorical(classe)
 
 classificador = Sequential()
-classificador.add(Dense())
-classificador.add(Dropout())
-classificador.compile(optimizer = '',loss = '', metrics = [''])
+classificador.add(Dense(units = 8, activation = 'tanh', kernel_initializer = 'normal', input_dim = 4))
+classificador.add(Dropout(0.1))
+classificador.add(Dense(units = 3, activation = 'softmax'))
+classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy',metrics = ['binary_accuracy'])
 
-classificador.compile(optimizer = 'adam' loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
+classificador.fit(x = previsores, y= classe_dummy, batch_size = 10, epochs=100)
 
-classificador.fit(x = previsores, y= classe, batch_size = , epochs= )
 
 
 classificador_json = classificador.to_json()
-with open('irisneuralnetwork','w'):
-    json_file.write(classificador_json)
-classificador.save_weights('irisweights.h5')
+with open ('classificador_breast.json','w') as json_file:
+    json_file.write(classificador_json) 
+classificador.save_weights('classificador_breast.h5')
 
+resultados = classificador.evaluate(previsores,classe_dummy)
